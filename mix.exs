4f9ef5500dbfeffbf003ef4f2af2fc2dd8121eee
Clay Sampson <pdgeek@gmail.com>
2020-07-06 09:29:31 -0400

    prep for publishing

--- mix.exs
+++ mix.exs
@@ -8,9 +8,7 @@ defmodule Distribunator.MixProject do
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      licenses: "MIT",
-      links: "https://github.com/sampscl/distribunator",
-      source_url: "https://github.com/sampscl/distribunator",
+      package: package(),
     ]
   end
 
@@ -27,6 +25,14 @@ defmodule Distribunator.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:ex_doc, "~> 0.21", only: :dev, runtime: false},
+    ]
+  end
+
+  defp package do
+    [
+      description: "Utilities supporting process distribution across nodes and node connections",
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/sampscl/distribunator"},
     ]
   end
 end
