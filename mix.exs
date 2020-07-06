f9b707341f0936afa938df538e980ab6a4eabe0c
Clay Sampson <pdgeek@gmail.com>
2020-07-06 09:53:32 -0400

    fixed version number, I really need a checklist

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Distribunator.MixProject do
   def project do
     [
       app: :distribunator,
-      version: "0.1.0",
+      version: "0.1.2",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
