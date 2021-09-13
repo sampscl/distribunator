fa22f3f7e96e1797213e8de9f297a92d601fcc73
Clay Sampson <pdgeek@gmail.com>
2021-09-13 16:53:04 -0400

    fix woops

--- lib/distribunator.ex
+++ lib/distribunator.ex
@@ -6,7 +6,6 @@ defmodule Distribunator do
 
   @impl Application
   def start(_type, _args) do
-    :pg.start_link()
     :ok = case :pg.start_link() do
       {:ok, _pid} -> :ok
       {:error, {:already_started, _pid}} -> :ok
