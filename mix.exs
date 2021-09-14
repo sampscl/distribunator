b2eef1812e602bdd665db6b27def75a5a17e6e09
Clay Sampson <pdgeek@gmail.com>
2021-09-14 10:17:17 -0400

    update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Distribunator.MixProject do
   def project do
     [
       app: :distribunator,
-      version: "1.2.0",
+      version: "1.2.1",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
